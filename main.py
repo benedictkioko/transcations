eeb73912ace13dbf172f7013839ee65f94b71604
Benedict Kioko <kiokobenedict@gmail.com>
2018-06-11 09:45:03 +0300

    modified main.py

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 import csv
 from operator import itemgetter
 from datetime import datetime
+from itertools import islice
 
 
-def find(transactions_csv_file_path, n = 0):
-  consecutive_days = {}
-  previous_day = {}
-  with open(transactions_csv_file_path, 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    next(reader)
-    for customer_id, _, date_str in reader:
-        date = datetime.strptime(date_str[:10], "%Y-%M-%d")
-        if customer_id not in consecutive_days:
-            consecutive_days[customer_id] = [1]
-        if customer_id not in previous_day:
-            previous_day[customer_id] = date
-        else:
-            td = date - previous_day[customer_id]
-            previous_day[customer_id] = date
-            if td.days == 1:
-                consecutive_days[customer_id][0] += 1
+def get_the_best(transactions_csv_file_path, n=0):
+    consecutive_days = {}
+    previous_day = {}
+    with open(transactions_csv_file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        # this line changed
+        for customer_id, _, date_str in sorted(reader, key=itemgetter(0, 2)):
+            date = datetime.strptime(date_str[:10], "%Y-%M-%d")
+            if customer_id not in consecutive_days:
+                consecutive_days[customer_id] = [1]
+            if customer_id not in previous_day:
+                previous_day[customer_id] = date
             else:
-                consecutive_days[customer_id].insert(0, 1)
+                td = date - previous_day[customer_id]
+                previous_day[customer_id] = date
+                if td.days == 1:
+                    consecutive_days[customer_id][0] += 1
+                elif td.days != 0:
+                    consecutive_days[customer_id].insert(0, 1)
 
-    longest_consecutive_days = []
-    for cust_id, vals in consecutive_days.items():
-        longest_consecutive_days.append((cust_id, max(vals)))
+        longest_consecutive_days = map(lambda x: (
+            x[0], max(x[1])), consecutive_days.items())
 
-         
-    sorted_consecutive_days = sorted(longest_consecutive_days, key=itemgetter(1), reverse=True)
-    return list(map(lambda x : x[0], sorted_consecutive_days))[:n]
+        sorted_consecutive_days = sorted(
+            longest_consecutive_days, key=itemgetter(1), reverse=True)
+        return list(islice(map(lambda x: x[0], sorted_consecutive_days), n))
 
 
 def main():
     file_path = input('file > ')
     n = int(input('num > '))
-    print(find(file_path, n))
+    print(get_the_best(file_path, n))
+
 
 if __name__ == '__main__':
     main()
